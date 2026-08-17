52307ae44eff0445094024ab1436dd33e7d74af4
Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>
2026-08-17 11:09:15 +0200

    Make TOTP optional, refactor/test auth routes
    1. allow operation without a TOTP DN for user/pass+certificate-only
       deployments
    2. factor certificate checks into a separate function for easier
       maintenance and testability
    3. add tests for the above to ensure the logic works as expected
    
    Signed-off-by: Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>

--- src/ldap.rs
+++ src/ldap.rs
@@ -21,8 +21,9 @@ use crate::auth_control::{write_auth_result, AuthControl};
 use crate::config;
 
 pub fn login_totp(runtime: &runtime::Runtime, config: &config::Config, auth_control_file: String, username: &str, password: &str, totp: &str) {
+    let dn_totp = config.dn_totp.as_ref().expect("login_totp requires dn_totp");
     let password = format!("{};{}", password, totp);
-    let dn = config.dn_totp.replacen("{}", &dn_escape(username), 1);
+    let dn = dn_totp.replacen("{}", &dn_escape(username), 1);
     check_credentials_async(runtime, config, auth_control_file, dn, password)
 }
 
